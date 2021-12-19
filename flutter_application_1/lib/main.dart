fc5029d76ca8bc5c21497fe55cb8032596050397
Mustafa Flah <93487876+MustafaFlah@users.noreply.github.com>
2021-12-19 20:22:23 +0300

    Uplod

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -68,13 +68,35 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body:ListView(children: [],)
-    
-    );
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              child: Card(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text("Premier league"),
+                        )
+                      ],
+                    ),
+                    Container(child: Card(
+                      child: Row(children: [
+                        Container(child: Image.asset("name"),)
+                      ],),
+                    ),)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
